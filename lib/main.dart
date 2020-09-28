44d0e26deea66d929b1d41f801e3450087ff6a9d
DOWGLAS <dowglasmaia@live.com>
2020-09-28 15:46:51 -0300

    Criando a Pagina e realizando o inport da mesma

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_buscador_giphys/views/HomePage.dart';
+
+
 
 void main() {
   runApp(MaterialApp(
-
+  home: HomePage()
+    ,
   ));
 }
+
